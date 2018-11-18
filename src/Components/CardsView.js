3e480a00cbb1f4e5a1dd83034de466d6126431dd
fqchenxin@gmail.com <fqchenxin@gmail.com>
2018-11-19 10:51:50 +1100

    cardsview wire up with server

--- src/Components/CardsView.js
+++ src/Components/CardsView.js
@@ -4,7 +4,7 @@ import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import SimpleCard from "./SimpleCard";
 import Pagination from "./Pagination";
-
+import axios from "axios";
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -12,14 +12,63 @@ const styles = theme => ({
     paddingRight: theme.spacing.unit * 30,
     paddingBottom: theme.spacing.unit * 3,
     paddingLeft: theme.spacing.unit * 30,
-    backgroundColor: "#eeeeee",
+    backgroundColor: "#eeeeee"
   },
   control: {
     padding: theme.spacing.unit * 2
   }
 });
-
+//const axios = require("axios");
 class CardsView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ""
+    };
+  }
+
+  componentDidMount() {
+    let self = this;
+    axios
+      .post(
+        "https://atr-test-dh1.aiam-dh.com/atr-gateway/identity-management/api/v1/auth/token",
+        {
+          password: "password",
+          username: "frontendtest"
+        }
+      )
+      .then(function(response) {
+        console.log(response.data.token);
+        self.setState({
+          token: response.data.token
+        });
+        axios
+          .get(
+            "https://atr-test-dh1.aiam-dh.com/atr-gateway/ticket-management/api/v1/tickets?ticketType=incident&sortDirection=DESC&page=0&perPage=10",
+            {
+              headers: {
+                apiToken:self.state.token,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+              }
+            }
+          )
+          .then(function(response) {
+            // handle success
+            console.log(response);
+          })
+          .catch(function(error) {
+            // handle error
+            console.log(error);
+          })
+          .then(function() {
+            // always executed
+          });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
   render() {
     const { classes } = this.props;
 
@@ -41,7 +90,7 @@ class CardsView extends Component {
             </Grid>
           </Grid>
           <Pagination />
-        </Grid> 
+        </Grid>
       </React.Fragment>
     );
   }
